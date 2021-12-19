cb0279d1f6c9f7f76588cc9148f9309d447037c8
Guilherme Santos <guilhermedev@hotmail.com>
2021-12-18 23:10:22 -0300

    feat erros

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -8,29 +8,14 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 
 const Footer = () => {
-  const [auth, setAuth] = React.useState(true);
-  const [anchorEl, setAnchorEl] = React.useState(null);
-
-  const handleChange = (event) => {
-    setAuth(event.target.checked);
-  };
-
-  const handleMenu = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
 
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
           <Typography component="div" sx={{ flexGrow: 1}}>
-            Desenvolvido com ❤️ por <a href='https://github.com/GuilhermeDev98' target="_blank" style={{color: 'white', textDecoration: 'none'}}>Guilherme Santos</a>
+            Desenvolvido com ❤️ por <a href='https://github.com/GuilhermeDev98' target="_blank" rel="noreferrer" style={{color: 'white', textDecoration: 'none'}}>Guilherme Santos</a>
           </Typography>
-          {auth && (
             <div>
               <IconButton
                 color="inherit"
@@ -43,7 +28,6 @@ const Footer = () => {
                 <LinkedInIcon />
               </IconButton>
             </div>
-          )}
         </Toolbar>
       </AppBar>
     </Box>
--- src/Components/VacancyCard.js
+++ src/Components/VacancyCard.js
@@ -1,11 +1,10 @@
-import React, {useEffect, useState} from 'react'
+import React, {useState} from 'react'
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import axios from 'axios';
 import Avatar from '@mui/material/Avatar';
 import ReactImage from '../Assets/Images/atom.png'
 
@@ -18,25 +17,15 @@ import DialogTitle from '@mui/material/DialogTitle';
 const VacancyCard = ({vaga}) => {
 
     const [open, setOpen] = useState(false);
-    const [Image, SetImage] = useState(null)
 
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
-
-    const GetImage = async () => {
-        const {data} = await axios.get("https://api.unsplash.com/photos/random/?query=developer&client_id=wmzRL3kL5acdHjKeXZA1K-nuuRcCoLIlJYutXSiH5L4")
-        SetImage(data.urls.small)
-    }
 
     const NewLine = (text) => {
         const newText = text.split('\n').map(str => <p>{str}</p>);
         return newText
     }
-    
-    useEffect(() => {
-        GetImage()
-    }, [])
-    console.log(Image)
+
     return (
         <Card sx={{ maxWidth: 300 }} style={{margin: '2%'}}>
             <CardMedia
